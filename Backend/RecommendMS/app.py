ef7e3e1803549425a1d7367338a9669f16d91db4
thaisrocine <thais.almeidacc@gmail.com>
2023-10-23 14:13:39 -0300

    commit recommend js

--- Backend/RecommendMS/app.py
+++ Backend/RecommendMS/app.py
@@ -4,7 +4,7 @@ import openai
 app = Flask(__name__)
 
 # Defina a chave de API do GPT-3
-openai.api_key = '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'
+openai.api_key = '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'
 
 
 # Rota para obter a lista de filmes
@@ -20,20 +20,49 @@ def recomendar_filme():
     genero = data.get('genero', 'terror')
 
     # Usando o GPT-3 para gerar uma recomendação de filme com base no sentimento e gênero
-    prompt = f"Recomendar um filme com sentimento {sentimento} e gênero {genero}"
+    prompt = f"Recomenda 5 filmes que tenha relacao com meu sentimento de {sentimento} e tenha um gênero {genero}. Quero em portugues"
 
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
         max_tokens=50,
-        n=1,
+        n=5,
         stop=None,
         temperature=0.7
     )
 
     recommended_movie = response.choices[0].text.strip()
 
+    print(recommended_movie)
+
     return jsonify({'recommended_movie': recommended_movie})
+
+
+@app.route('/filmes/recomendar/nova', methods=['POST'])
+def recomendar_filme_nova():
+    data = request.get_json()
+    sentimento = data.get('sentimento', 'alegria')
+    genero = data.get('genero', 'terror')
+
+    prompt = f"Estou com um sentimento {sentimento} e querendo assistir um filme de gênero {genero}"
+
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=200, 
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+
+    recommended_movie_text = response.choices[0].text.strip()
+
+    # Divida a string em uma lista de filmes
+    recommended_movies_list = [movie.strip() for movie in recommended_movie_text.split('\n') if movie.strip()]
+
+    return jsonify({'recommended_movies': recommended_movies_list})
+
+
 
 if __name__ == '__main__':
     app.run(port=8000, host='localhost', debug=True)
